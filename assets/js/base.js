7805a2e7a01e039539135f16c028251e4be59643
茶心 <2953015964@qq.com>
2022-11-14 19:54:33 +0800

    完成了后台页面的功能

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,6 +1,20 @@
 $(function () {
     $.ajaxPrefilter(function (option) {
         option.url = "http://www.liulongbin.top:3007" + option.url
+        //为有权限的接口设置请求头
+        if (option.url.indexOf("/my/") !== -1) {
+            option.headers = {
+                Authorization: localStorage.getItem("token") || ""
+            }
+        }
+        //发起ajax请求时，立即验证是否通过了身份验证，没有的话返回登录页面
+        option.complete = function (res) {
+            if (res.responseJSON.status === 1 || res.responseJSON.message == "身份认证失败！") {
+                localStorage.removeItem('token')
+                location.href = "login.html"
+
+            }
+        }
 
     })
 })
